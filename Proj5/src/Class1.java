26f144cf5aec2b4fd0a11f5e176f1c0c329bfac5
awartania <sqawartani@hotmail.com>
2019-05-23 16:33:36 -0400

    m

--- Proj5/src/Class1.java
+++ Proj5/src/Class1.java
@@ -5,7 +5,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World5");
+		System.out.println("Hello World 10");
 		
 		
 	}
